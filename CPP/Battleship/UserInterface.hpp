519a214171e769a3b505e58e084293950ccf77fa
reaped-git <reaperrighty@gmail.com>
2026-01-18 19:56:10 +0700

    Добавлены поле и интерфейс

---
+++ CPP/Battleship/UserInterface.hpp
@@ -0,0 +1,27 @@
+﻿#pragma once
+
+#include "Player.hpp"
+#include <string>
+
+namespace Battleship {
+	// Предварительное объявление
+	class GameManager;
+
+	class UserInterface
+	{
+	public:
+		// конструкторы и деконструктор
+		UserInterface(GameManager* manager);
+		~UserInterface() = default;
+
+		// публичные методы
+		void DisplayBoards(Player* player);
+		void DisplayMessage(const std::string& message);
+		void ShowGameOver(const std::string& winnerName);
+		void DisplayLegend();
+
+	private:
+		// приватные переменные
+		GameManager* m_gameManager;
+	};
+}
